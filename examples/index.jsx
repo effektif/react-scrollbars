fa284942d53fe6dea741a401895c4a8ad71d4750
Kristian <kristian.hamilton@signavio.com>
2020-09-24 12:38:53 +0200

    doc: get example page working, correct broken links

--- examples/index.jsx
+++ examples/index.jsx
@@ -1,24 +1,10 @@
-'use strict'
-var React = require('react');
+"use strict";
+var React = require("react");
+var ReactDOM = require("react-dom");
 
-var Home = require('./views/home/index.jsx');
+var Home = require("./views/home/index.jsx");
 
-require('normalize.css/normalize.css');
-require('./app.css');
+require("normalize.css/normalize.css");
+require("./app.css");
 
-var App = React.createClass({
-  render: function () {
-    return (
-      <div className="App">
-        <header className="App-header">
-        </header>
-
-        <div className="App-content">
-          <Home />
-        </div>
-      </div>
-    );
-  }
-});
-
-React.render(<App />, document.body);
+ReactDOM.render(<Home />, document.getElementById("App"));
